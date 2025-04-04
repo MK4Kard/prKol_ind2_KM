8139858439ccfc7d62667c994843c2bfc7af663a
MK4Kard <misha212032@gmail.com>
2025-04-04 17:21:06 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace prKol_ind2_4_KM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите навзвание файла");
+            string name = Console.ReadLine();
+
+            if (File.Exists(name))
+            {
+                string[] file = File.ReadAllLines(name);
+
+                Queue<int> number = new Queue<int>();
+
+                var nums = file.SelectMany(n => n.Split(' '));
+
+                foreach (var num in nums)
+                {
+                    if (int.TryParse(num, out int result))
+                    {
+                        number.Enqueue(Convert.ToInt32(num));
+                    }
+                    else
+                    {
+                        Console.WriteLine("В файле есть не число");
+                        Console.ReadLine();
+                        Environment.Exit(0);
+                    }
+                }
+
+                var order_num = number.OrderBy(n => n < 0);
+
+                foreach (var or_n in order_num)
+                {
+                    Console.WriteLine(or_n);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Несуществующий файл");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
